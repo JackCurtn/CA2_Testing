d2cff1ca4e9d855dacd343ee3d8cc720c697211b
Jack Curtin <82333861+JackCurtn@users.noreply.github.com>
2024-01-28 12:28:22 +0000

    Commit on main branch adding a calculator to the hello.py file

--- hello.py
+++ hello.py
@@ -4,3 +4,13 @@ fancyString = "Fancy string for testing"
 
 for letter in fancyString:
     print(letter)
+
+# A very advanced calculator
+def addNumbers(x,y):
+    return x + y
+
+
+x = 5
+y = 6
+
+print(addNumbers(5,6))
